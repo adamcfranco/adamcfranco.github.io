022261a1cda89f122e7aad62fa04d0dbba745fa2
Adam Franco <adamcfranco@outlook.com>
2018-05-27 16:00:04 -0600

    Deleted some nonsense

--- .to-be-organized/modal_animations/modal.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('[data-modal]').click(function () {
-		var id = $(this).data("modal");
-		$("#"+id).addClass("active");
-	});
-	$('.md-close').click(function() {
-		$(this).closest('.md').removeClass("active");
-	});
-});
